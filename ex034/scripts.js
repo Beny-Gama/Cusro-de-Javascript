8e3d339618629bfd77248f7cc7516706f2bd3add
Bernardo Gama <bgama18@gmail.com>
2024-04-28 16:02:16 -0400

    caminhado

--- ex034/scripts.js
+++ ex034/scripts.js
@@ -1,1 +1,25 @@
-// Clonando objetos
+// clonagem de Objeto
+// Para colnar um objeto vc so tem que usar = Object.assign({},obejto há ser clonar.) 
+
+const celular = {
+    marcaCelular : 'Asus',
+    tamanhoTela: {
+        velocidade: 155,
+        horizontal: 75
+    },
+    ligar: function () {
+        console.log('Fazendo Ligação...');
+    }
+}
+
+const novoObjeto = Object.assign({
+    bateria: 5000
+},celular);
+console.log(novoObjeto);
+
+
+// outra forma de clonar um objeto é = {...}
+
+const objeto2 = {...celular};
+
+console.log(objeto2)
---
+++ ex035/scripts.js
@@ -0,0 +1,4 @@
+// Math
+// math é uma bibliota do JS para pazer operaões matematicas
+// mezila: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
